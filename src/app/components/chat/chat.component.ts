c4a4c632744366e31717ba0d9c014198990c6c99
Pedro Conde <pconde007@gmail.com>
2022-11-24 18:15:18 +0000

    Se realiza el envío y recepción de mensajes

---
+++ src/app/components/chat/chat.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ChatService } from '../../services/chat.service';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit, OnDestroy {
+
+  texto = '';
+  mensajesSubscription!: Subscription;
+  elemento!: HTMLElement;
+
+  mensajes: any[] = [];
+
+  constructor(public chatService:ChatService) { }  
+
+  ngOnInit() {
+
+    this.elemento = <HTMLInputElement>document.getElementById('chat-mensajes');
+    this.mensajesSubscription = this.chatService.getMessages().subscribe( msg => {
+      
+      this.mensajes.push(msg);
+
+      setTimeout(() => {
+        this.elemento.scrollTop = this.elemento.scrollHeight;
+      }, 50);
+    });
+  }
+
+  ngOnDestroy() {
+    this.mensajesSubscription.unsubscribe();
+  }
+
+  enviar() {
+
+    if(this.texto.trim().length === 0){
+      return;
+    }
+
+    this.chatService.sendMessage(this.texto);
+    this.texto = '';
+  }
+
+}
